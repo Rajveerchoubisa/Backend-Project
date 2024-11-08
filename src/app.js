9f69d46b39a85c6d88672db0c32e3428439f0e6f
Rajveerchoubisa <ranveer0596@gmail.com>
2024-11-08 16:12:11 +0530

    new things

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// three major configuration 
+app.use(express.json({limit: "16kb"})) // to accept the json data 
+app.use(express.urlencoded({extended: true,limit: "16kb"})) // url improvement like %20 and + between url search
+app.use(express.static("public")) // public assets for storing the photo files in server
+
+app.use(cookieParser())  
+
+
+export {app}
